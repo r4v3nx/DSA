c1466b1c7d659ed824dca5c7a2e6e75fa5cccbb6
r4v3nx <131524710+r4v3nx@users.noreply.github.com>
2023-06-06 07:34:40 +0530

    Create orderAgnosticBS.java

---
+++ orderAgnosticBS.java
@@ -0,0 +1,38 @@
+class orderAgnosticBS {
+    public static void main(String[] args) {
+        int[] arr = {902,422,213,87,15,1};
+        System.out.println(binarySearch(arr,15));
+    }
+static int binarySearch(int[] arr,int target){
+    
+    int start = 0;
+    int end = arr.length-1;
+    boolean isAsc = arr[start] < arr[end];
+    while(start<end){
+    int mid = start+(end-start/2);
+    
+    if(target==arr[mid]){
+        return mid;
+    }
+    if(isAsc){
+    if(target<arr[mid]){
+    end = mid-1;
+    }
+    else{
+        start = mid+1;
+    }
+    }
+    else{
+        if(target>arr[mid]){
+    end = mid-1;
+    }
+    else{
+        start = mid+1;
+    }
+    }
+
+    
+}
+return -1;
+}
+}
