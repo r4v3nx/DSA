e6894e97a6e817eff1e7f31f73f0347decaedff9
r4v3nx <131524710+r4v3nx@users.noreply.github.com>
2023-07-25 21:53:12 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,20 @@
+//pattern printing
+class Solution {
+
+    void printSquare(int n) {
+        
+        for(int i = 0; i<n; i++){
+            for(int j =0; j<n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+/* 
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * * 
+*/
