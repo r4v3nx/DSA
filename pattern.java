0e39ea8377dada0749b484e657901dba0e97ae46
r4v3nx <131524710+r4v3nx@users.noreply.github.com>
2023-07-25 22:21:46 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -10,7 +10,6 @@ class Solution {
             System.out.println();
         }
     }
-}
 /* 
 * * * * *
 * * * * *
@@ -18,3 +17,50 @@ class Solution {
 * * * * *
 * * * * * 
 */
+
+    void printTriangle(int n) {
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+/*
+*
+* *
+* * *
+* * * *
+* * * * *
+*/
+    void printTriangle(int n) {
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+/*
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+*/
+void printTriangle(int n) {
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            System.out.print(i+1 + " ");
+            }
+            System.out.println();
+        }
+    }
+/*
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+*/
+}
