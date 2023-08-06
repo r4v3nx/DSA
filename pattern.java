2f55eb9476748cc4498e620cecbc562f5aa7b2b6
r4v3nx <131524710+r4v3nx@users.noreply.github.com>
2023-08-06 10:55:01 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -63,4 +63,33 @@ void printTriangle(int n) {
 4 4 4 4
 5 5 5 5 5
 */
+
+void printDiamond(int n) {
+        // Your code here
+for(int i=0; i<n; i++){
+    for(int j=0; j<n-i-1; j++){
+        System.out.print(" ");
+    }
+    for(int k=0; k<i+1; k++){
+        System.out.print("* ");
+    }
+    for(int l=0; l<n-i-2; l++){
+        System.out.print(" ");
+    }
+    System.out.println();
+    
+}
+for(int m=0; m<n; m++){
+    for(int y=0; y<m; y++){
+        System.out.print(" ");
+    }
+    for(int x=0; x<n-m; x++){
+        System.out.print("* ");
+    }
+    for(int z=0; z<m-1; z++){
+        System.out.print(" ");
+    }
+    System.out.println();
+}
+    }
 }
